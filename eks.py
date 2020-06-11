ef15dba8f32f5aa3cc73ab301e133e4427ed1287
jbennett7 <joe.bennett@contino.io>
2020-06-10 23:20:40 -0400

    restructured repository and consolidated security group ip policies.

--- eks.py
+++ eks.py
@@ -1,20 +1,16 @@
 from boto3 import client
 from time import sleep
 from jinja2 import Template
-from os import listdir
 import json
 
 VPC_ID="vpc-0438aaee9ab9f4c25"
 SUBNET_ID="subnet-0eb4bad3b692c74ef"
 EKS_ASSUME_ROLE_POLICY_DOCUMENT_PATH="policies/eks_assume.json"
-CONTROL_PLANE_INGRESS="sg_ippermissions/control_plane_ingress.json.j2"
-CONTROL_PLANE_EGRESS="sg_ippermissions/control_plane_egress.json.j2"
-WORKER_NODE_INGRESS="sg_ippermissions/worker_node_ingress.json.j2"
-WORKER_NODE_EGRESS="sg_ippermissions/worker_node_egress.json.j2"
-BASTION_HOST_INGRESS="sg_ippermissions/bastion_host_ingress.json.j2"
-BASTION_HOST_EGRESS="sg_ippermissions/bastion_host_egress.json.j2"
-ALB_INGRESS="sg_ippermissions/alb_ingress.json.j2"
-ALB_EGRESS="sg_ippermissions/alb_egress.json.j2"
+
+CONTROL_PLANE_INGRESS="sg_policies/control_plane_ingress.json.j2"
+WORKER_NODE_INGRESS="sg_policies/worker_node_ingress.json.j2"
+BASTION_HOST_INGRESS="sg_policies/bastion_host_ingress.json.j2"
+ALB_INGRESS="sg_policies/alb_ingress.json.j2"
 
 def sleep1(): sleep(.25)
 def sleep2(): sleep(1)
@@ -86,99 +82,33 @@ class AWSEks(object):
         for sg in [control_sg_id, worker_sg_id, bastion_sg_id, alb_sg_id]:
             self._append_to_objs('sg', sg)
 
-    def authorize_control_plane_security_group_ingress(self):
+    def authorize_security_group_policies(self):
         control_sg_id, worker_sg_id, bastion_sg_id, alb_sg_id = self._get_security_groups()
-        with open(CONTROL_PLANE_INGRESS) as f:
-            data = f.read()
-        ec2().authorize_security_group_ingress(
-            GroupId=control_sg_id,
-            IpPermissions=json.loads(Template(data).render(
-                bastion_sg_id=bastion_sg_id,
-                worker_sg_id=worker_sg_id,
-                alb_sg_id=alb_sg_id)))
+        for sg in [(control_sg_id, CONTROL_PLANE_INGRESS), (worker_sg_id, WORKER_NODE_INGRESS),
+            (bastion_sg_id, BASTION_HOST_INGRESS), (alb_sg_id, ALB_INGRESS)]:
+                with open(sg[1]) as f:
+                    data = f.read()
+                ec2().authorize_security_group_ingress(
+                    GroupId=sg[0],
+                    IpPermissions=json.loads(Template(data).render(
+                        bastion_sg_id=bastion_sg_id,
+                        worker_sg_id=worker_sg_id,
+                        control_sg_id=control_sg_id,
+                        alb_sg_id=alb_sg_id)))
 
-    def authorize_control_plane_security_group_egress(self):
+    def revoke_security_group_policies(self):
         control_sg_id, worker_sg_id, bastion_sg_id, alb_sg_id = self._get_security_groups()
-        with open(CONTROL_PLANE_EGRESS) as f:
-            data = f.read()
-        ec2().authorize_security_group_egress(
-            GroupId=control_sg_id,
-            IpPermissions=json.loads(Template(data).render(
-                worker_sg_id=worker_sg_id)))
-
-    def authorize_worker_node_security_group_ingress(self):
-        control_sg_id, worker_sg_id, bastion_sg_id, alb_sg_id = self._get_security_groups()
-        with open(WORKER_NODE_INGRESS) as f:
-            data = f.read()
-        ec2().authorize_security_group_ingress(
-            GroupId=control_sg_id,
-            IpPermissions=json.loads(Template(data).render(
-                worker_sg_id=worker_sg_id,
-                control_sg_id=control_sg_id)))
-
-    def authorize_bastion_host_security_group_ingress(self):
-        control_sg_id, worker_sg_id, bastion_sg_id, alb_sg_id = self._get_security_groups()
-        with open(BASTION_HOST_INGRESS) as f:
-            data = f.read()
-        ec2().authorize_security_group_ingress(
-            GroupId=bastion_sg_id,
-            IpPermissions=json.loads(Template(data).render()))
-
-    def authorize_alb_security_group_ingress(self):
-        control_sg_id, worker_sg_id, bastion_sg_id, alb_sg_id = self._get_security_groups()
-        with open(ALB_INGRESS) as f:
-            data = f.read()
-        ec2().authorize_security_group_ingress(
-            GroupId=alb_sg_id,
-            IpPermissions=json.loads(Template(data).render(
-                control_sg_id=control_sg_id)))
-
-    def revoke_control_plane_security_group_ingress(self):
-        control_sg_id, worker_sg_id, bastion_sg_id, alb_sg_id = self._get_security_groups()
-        with open(CONTROL_PLANE_INGRESS) as f:
-            data = f.read()
-        ec2().revoke_security_group_ingress(
-            GroupId=control_sg_id,
-            IpPermissions=json.loads(Template(data).render(
-                bastion_sg_id=bastion_sg_id,
-                worker_sg_id=worker_sg_id,
-                alb_sg_id=alb_sg_id)))
-
-    def revoke_control_plane_security_group_egress(self):
-        control_sg_id, worker_sg_id, bastion_sg_id, alb_sg_id = self._get_security_groups()
-        with open(CONTROL_PLANE_EGRESS) as f:
-            data = f.read()
-        ec2().revoke_security_group_egress(
-            GroupId=control_sg_id,
-            IpPermissions=json.loads(Template(data).render(
-                worker_sg_id=worker_sg_id)))
-
-    def revoke_worker_node_security_group_ingress(self):
-        control_sg_id, worker_sg_id, bastion_sg_id, alb_sg_id = self._get_security_groups()
-        with open(WORKER_NODE_INGRESS) as f:
-            data = f.read()
-        ec2().revoke_security_group_ingress(
-            GroupId=control_sg_id,
-            IpPermissions=json.loads(Template(data).render(
-                worker_sg_id=worker_sg_id,
-                control_sg_id=control_sg_id)))
-
-    def revoke_bastion_host_security_group_ingress(self):
-        control_sg_id, worker_sg_id, bastion_sg_id, alb_sg_id = self._get_security_groups()
-        with open(BASTION_HOST_INGRESS) as f:
-            data = f.read()
-        ec2().revoke_security_group_ingress(
-            GroupId=bastion_sg_id,
-            IpPermissions=json.loads(Template(data).render()))
-
-    def revoke_alb_security_group_ingress(self):
-        control_sg_id, worker_sg_id, bastion_sg_id, alb_sg_id = self._get_security_groups()
-        with open(ALB_INGRESS) as f:
-            data = f.read()
-        ec2().revoke_security_group_ingress(
-            GroupId=alb_sg_id,
-            IpPermissions=json.loads(Template(data).render(
-                control_sg_id=control_sg_id)))
+        for sg in [(control_sg_id, CONTROL_PLANE_INGRESS), (worker_sg_id, WORKER_NODE_INGRESS),
+            (bastion_sg_id, BASTION_HOST_INGRESS), (alb_sg_id, ALB_INGRESS)]:
+                with open(sg[1]) as f:
+                    data = f.read()
+                ec2().revoke_security_group_ingress(
+                    GroupId=sg[0],
+                    IpPermissions=json.loads(Template(data).render(
+                        bastion_sg_id=bastion_sg_id,
+                        worker_sg_id=worker_sg_id,
+                        control_sg_id=control_sg_id,
+                        alb_sg_id=alb_sg_id)))
 
     def delete_security_groups(self):
         for sg in self.objs['sg']:
@@ -197,16 +127,8 @@ if __name__ == '__main__':
     eks = AWSEks(vpc_id=VPC_ID)
     eks.create_eks_role()
     eks.create_security_groups()
-    eks.authorize_control_plane_security_group_ingress()
-    eks.authorize_control_plane_security_group_egress()
-    eks.authorize_worker_node_security_group_ingress()
-    eks.revoke_worker_node_security_group_ingress()
-    eks.authorize_alb_security_group_ingress()
-    eks.revoke_alb_security_group_ingress()
-    eks.authorize_bastion_host_security_group_ingress()
-    eks.revoke_bastion_host_security_group_ingress()
-    eks.revoke_control_plane_security_group_ingress()
-    eks.revoke_control_plane_security_group_egress()
+    eks.authorize_security_group_policies()
+    eks.revoke_security_group_policies()
     print(eks.objs)
     eks.delete_security_groups()
     eks.delete_eks_role()
